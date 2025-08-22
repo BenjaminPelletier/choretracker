d780422c3259340656ff2f81be3f29b9cb25a52c
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2025-08-22 23:11:30 +0000

    [feature] add index page template and static styling (#3)

--- choretracker/app.py
+++ choretracker/app.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+BASE_PATH = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+app.mount("/static", StaticFiles(directory=str(BASE_PATH / "static")), name="static")
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
